800b31941aa3c7795611f5c33c7cf5e5e0ece4c1
Ben Tranter <bwtrante@lakeheadu.ca>
2015-01-17 09:24:56 -0500

    Create tables for testing

--- web/server/db.js
+++ web/server/db.js
@@ -24,11 +24,23 @@ var dbConfig = {
 // -----------------
 
 module.exports.setup = function() {
+
+	// Setup, connect, create
 	r.connect({
 		host: dbConfig.host,
 		port: dbConfig.port
 	}, function(err, conn) {
 
+		// Log connection
 		console.log('Connected to ' + chalk.green('RethinkDB') + ' on port ' + chalk.blue(dbConfig.port));
+
+		// Create tables from config file
+		r.dbCreate(dbConfig.db).run(conn, function(err, result) {
+			if(err) {
+				console.log(chalk.red('Error: ') + 'database ' + dbConfig.db + ' already exists!');
+			} else {
+				console.log(chalk.blue('Rethink database ') + chalk.bold.dbConfig.db + chalk.blue(' created.'));
+			}
+		});
 	});
 };
