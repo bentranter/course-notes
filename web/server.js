aba8e36eebc8b1934ccd1a938dba4b148af677e2
Ben Tranter <bwtrante@lakeheadu.ca>
2015-02-11 21:22:33 -0500

    Add CORS support for "X-Access-Token" header

--- web/server.js
+++ web/server.js
@@ -58,7 +58,8 @@ app.use(bodyParser.json());
 // Enable CORS
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Access-Token');
   next();
 });
 // Only expose tests in development mode
