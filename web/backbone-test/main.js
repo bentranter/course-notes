70570e9213d3aecf3155b9b7d148cd088dbbc954
Ben Tranter <bwtrante@lakeheadu.ca>
2015-03-01 22:34:36 -0500

    Write docs

--- web/backbone-test/main.js
+++ web/backbone-test/main.js
@@ -3,30 +3,34 @@
   'use strict';
 
   var People = Backbone.Model.extend({
-    url: 'http://localhost:3000/api/people'
+    url: 'http://localhost:3000/api/people',
+    type: 'Test',
+    initialize: function() {
+      console.log('Initialized a new People model instance');
+    },
+    save: function() {
+      var data = document.getElementById('noteBody').innerHTML;
+      todoItem.set({description: data});
+      console.log(todoItem.get('description'));
+    },
+    toggleSidebar: function() {
+
+      var sidebar = document.getElementById('main');
+      
+      if (sidebar.classList.contains('ml220')) {
+        sidebar.classList.remove('ml220');
+      } else {
+        sidebar.classList.add('ml220');
+      }
+    }
   });
 
-  var save = function() {
-    var data = document.getElementById('noteBody').innerHTML;
-    todoItem.set({description: data});
-    console.log(todoItem.get('description'));
-  };
-
-  var toggleSidebar = function() {
-
-    var sidebar = document.getElementById('sidebar');
-    
-    if (sidebar.classList.contains('ml220')) {
-      sidebar.classList.remove('ml220');
-    } else {
-      sidebar.classList.add('ml220');
-    }
-  };
+  var test = new People();
 
   /**
    * Event Listeners
    */
-  document.getElementById('save').addEventListener('click', save);
-  document.getElementById('toggleSidebar').addEventListener('click', toggleSidebar);
+  document.getElementById('save').addEventListener('click', test.save);
+  document.getElementById('toggleSidebar').addEventListener('click', test.toggleSidebar);
 
 })();
